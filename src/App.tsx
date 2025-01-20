45ecdf63a74e5fc50aa2e18174cd7a34871d4640
Janne <jabbekeipert@gmail.com>
2025-01-20 12:46:12 +0100

    add browser router and query client provider

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { BrowserRouter } from "react-router";
+
 const App = () => {
-    return <h1>Medals</h1>
-}
+  const queryClient = new QueryClient();
+  return (
+    <QueryClientProvider client={queryClient}>
+      <BrowserRouter>
+        <h1>Medals</h1>
+      </BrowserRouter>
+    </QueryClientProvider>
+  );
+};
 
 export default App;
