d7537fc87400d2582866ecf4c4c557f3824f8e45
Janne <jabbekeipert@gmail.com>
2025-01-20 13:08:35 +0100

    remove test files

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,5 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter } from "react-router";
-import TestPage from "./pages/test";
 
 const App = () => {
   const queryClient = new QueryClient();
@@ -8,7 +7,6 @@ const App = () => {
     <QueryClientProvider client={queryClient}>
       <BrowserRouter>
         <h1>Medals</h1>
-        <TestPage />
       </BrowserRouter>
     </QueryClientProvider>
   );
