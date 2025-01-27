97c1ab21a99dae0bb4980f4771732ed7abf85668
Janne <jabbekeipert@gmail.com>
2025-01-27 15:49:42 +0100

    implement i18n on not found page

--- src/pages/NotFound/NotFoundPage.tsx
+++ src/pages/NotFound/NotFoundPage.tsx
@@ -1,6 +1,9 @@
 import { Box, Typography } from "@mui/joy";
+import { useTranslation } from "react-i18next";
 
 const NotFoundPage = () => {
+  const {t} = useTranslation()
+
   return (
     <Box
       sx={{
@@ -12,10 +15,10 @@ const NotFoundPage = () => {
       }}
     >
       <Typography level={"h1"} padding={"1.5rem"}>
-        404
+        {t("pages.notFound.h1")}
       </Typography>
       <Typography level={"h2"} color={"neutral"}>
-        Page Not Found
+        {t("pages.notFound.h2")}
       </Typography>
     </Box>
   );
