dd62b288d330374bbe9185ec24a1dcb14e0c692e
Janne <jabbekeipert@gmail.com>
2025-01-27 14:35:20 +0100

    homepage add i18n

--- src/pages/Home/HomePage.tsx
+++ src/pages/Home/HomePage.tsx
@@ -1,6 +1,9 @@
 import { Box, Chip, Typography } from "@mui/joy";
+import { useTranslation } from "react-i18next";
 
 const HomePage = () => {
+  const {t} = useTranslation();
+
   return (
     <Box
       sx={{
@@ -12,17 +15,12 @@ const HomePage = () => {
       }}
     >
       <Chip size="lg" sx={{ aspectRatio: 1 }}>
-        {" "}
         <Typography level={"h1"} fontSize={100}>
           🥇
         </Typography>
       </Chip>
-      <Typography level={"h1"} padding={"1.5rem"} textAlign={"center"}>
-        Medals
-      </Typography>
-      <Typography level={"h4"} color={"neutral"} textAlign={"center"}>
-        The soon to be Medal Management System
-      </Typography>
+      <Typography level={"h1"} padding={"1.5rem"} textAlign={"center"}>{t("pages.homePage.h1")}</Typography>
+      <Typography level={"h4"} color={"neutral"} textAlign={"center"}>{t("pages.homePage.h3")}</Typography>
     </Box>
   );
 };
