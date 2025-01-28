36cefbcaa831fbf1d445e006c0889e982bf41ea9
Janne <jabbekeipert@gmail.com>
2025-01-28 10:23:13 +0100

    extract language selector

---
+++ src/components/NavBar/LanguageSelector.tsx
@@ -0,0 +1,109 @@
+import { LanguageOutlined, KeyboardArrowDown } from "@mui/icons-material";
+import {
+  ListItem,
+  ListItemButton,
+  ListItemContent,
+  Typography,
+  List,
+  Box,
+} from "@mui/joy";
+import { Fragment, useState } from "react";
+import { useTranslation } from "react-i18next";
+
+const Toggler = ({
+  defaultExpanded = false,
+  overridenOpen = false,
+  renderToggle,
+  children,
+}: {
+  defaultExpanded?: boolean;
+  overridenOpen?: boolean;
+  children: React.ReactNode;
+  renderToggle: (params: {
+    open: boolean;
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
+  }) => React.ReactNode;
+}) => {
+  const [open, setOpen] = useState(defaultExpanded);
+  return (
+    <Fragment>
+      {renderToggle({ open, setOpen })}
+      <Box
+        sx={[
+          {
+            display: "grid",
+            transition: "0.2s ease",
+            "& > *": {
+              overflow: "hidden",
+            },
+          },
+          (overridenOpen ?? open)
+            ? { gridTemplateRows: "1fr" }
+            : { gridTemplateRows: "0fr" },
+        ]}
+      >
+        {children}
+      </Box>
+    </Fragment>
+  );
+};
+
+const LanguageSelector = () => {
+  const { t, i18n } = useTranslation();
+  const [open, setOpen] = useState(false);
+
+  return (
+    <ListItem
+      nested
+      sx={{
+        display: "flex",
+        flexDirection: "column-reverse",
+      }}
+    >
+      <Toggler
+        overridenOpen={open}
+        renderToggle={() => (
+          <ListItemButton onClick={() => setOpen(!open)}>
+            <LanguageOutlined />
+            <ListItemContent>
+              <Typography level="title-sm">
+                {t("components.navbar.languageSelector")}
+              </Typography>
+            </ListItemContent>
+            <KeyboardArrowDown
+              sx={[
+                open
+                  ? {
+                      transform: "none",
+                      transition: "ease .3s transform",
+                    }
+                  : {
+                      transform: "rotate(-180deg)",
+                      transition: "ease .3s transform",
+                    },
+              ]}
+            />
+          </ListItemButton>
+        )}
+      >
+        <List sx={{ gap: 0.5 }}>
+          {Object.keys(i18n.options.resources ?? []).map((language) => (
+            <ListItem>
+              <ListItemButton
+                onClick={() => {
+                  i18n.changeLanguage(language);
+                  setOpen(false);
+                }}
+                selected={i18n.language == language}
+              >
+                <ListItemContent>{t("languages." + language)}</ListItemContent>
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Toggler>
+    </ListItem>
+  );
+};
+
+export default LanguageSelector;
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -2,12 +2,9 @@ import ColorSchemeToggle from "@components/ColorSchemeToggle/ColorSchemeToggle";
 import InfoCard from "@components/InfoCard/InfoCard";
 import useSidebar from "@hooks/useSidebar";
 import {
-  HomeRounded,
-  KeyboardArrowDown,
-  LanguageOutlined,
-  LogoutRounded,
+  HomeRounded, LogoutRounded,
   PeopleRounded,
-  SearchRounded,
+  SearchRounded
 } from "@mui/icons-material";
 import {
   Avatar,
@@ -24,10 +21,8 @@ import {
   Sheet,
   Typography,
 } from "@mui/joy";
-import { useState } from "react";
 import { useTranslation } from "react-i18next";
 import { matchPath, useLocation, useNavigate } from "react-router";
-import { Fragment } from "react/jsx-runtime";
 
 const navBarElements = [
   {
@@ -39,102 +34,6 @@ const navBarElements = [
     icon: <PeopleRounded />,
   },
 ];
-
-const Toggler = ({
-  defaultExpanded = false,
-  overridenOpen = false,
-  renderToggle,
-  children,
-}: {
-  defaultExpanded?: boolean;
-  overridenOpen?: boolean;
-  children: React.ReactNode;
-  renderToggle: (params: {
-    open: boolean;
-    setOpen: React.Dispatch<React.SetStateAction<boolean>>;
-  }) => React.ReactNode;
-}) => {
-  const [open, setOpen] = useState(defaultExpanded);
-  return (
-    <Fragment>
-      {renderToggle({ open, setOpen })}
-      <Box
-        sx={[
-          {
-            display: "grid",
-            transition: "0.2s ease",
-            "& > *": {
-              overflow: "hidden",
-            },
-          },
-          (overridenOpen ?? open)
-            ? { gridTemplateRows: "1fr" }
-            : { gridTemplateRows: "0fr" },
-        ]}
-      >
-        {children}
-      </Box>
-    </Fragment>
-  );
-};
-
-const LanguageSelector = () => {
-  const { t, i18n } = useTranslation();
-  const [open, setOpen] = useState(false);
-
-  return (
-    <ListItem
-      nested
-      sx={{
-        display: "flex",
-        flexDirection: "column-reverse",
-      }}
-    >
-      <Toggler
-        overridenOpen={open}
-        renderToggle={() => (
-          <ListItemButton onClick={() => setOpen(!open)}>
-            <LanguageOutlined />
-            <ListItemContent>
-              <Typography level="title-sm">
-                {t("components.navbar.languageSelector")}
-              </Typography>
-            </ListItemContent>
-            <KeyboardArrowDown
-              sx={[
-                open
-                  ? {
-                      transform: "none",
-                      transition: "ease .3s transform",
-                    }
-                  : {
-                      transform: "rotate(-180deg)",
-                      transition: "ease .3s transform",
-                    },
-              ]}
-            />
-          </ListItemButton>
-        )}
-      >
-        <List sx={{ gap: 0.5 }}>
-          {Object.keys(i18n.options.resources ?? []).map((language) => (
-            <ListItem>
-              <ListItemButton
-                onClick={() => {
-                  i18n.changeLanguage(language);
-                  setOpen(false);
-                }}
-                selected={i18n.language == language}
-              >
-                <ListItemContent>{t("languages." + language)}</ListItemContent>
-              </ListItemButton>
-            </ListItem>
-          ))}
-        </List>
-      </Toggler>
-    </ListItem>
-  );
-};
 
 const NavBar = () => {
   const { collapseSidebar, sideBarExtended } = useSidebar();
