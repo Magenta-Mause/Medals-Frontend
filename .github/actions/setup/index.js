9903b3cd8c88f3afb0b250e6f4e41d47464c5937
Eco_Fresh_Kaese <alexander.jablo@gmail.com>
2025-01-30 14:38:11 +0100

    fix action

--- .github/actions/setup/index.js
+++ .github/actions/setup/index.js
@@ -1,12 +1,12 @@
-import { getInput } from "@actions/core";
-import { exec } from "@actions/exec";
+const core = require("@actions/core");
+const exec = require("@actions/exec");
 
 async function run() {
   try {
-    const nodeVersion = getInput("node-version");
-    await exec(`nvm install ${nodeVersion}`);
-    await exec(`nvm use ${nodeVersion}`);
-    await exec(`nvm ci`);
+    const nodeVersion = core.getInput("node-version");
+    await exec.exec(`nvm install ${nodeVersion}`);
+    await exec.exec(`nvm use ${nodeVersion}`);
+    await exec.exec(`nvm ci`);
   } catch (error) {
     core.setFailed(error.message);
   }
