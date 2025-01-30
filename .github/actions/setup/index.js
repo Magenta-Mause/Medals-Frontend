facc4d941396edd6a3ae0d1895f5b6408f06de32
Eco_Fresh_Kaese <alexander.jablo@gmail.com>
2025-01-30 14:34:45 +0100

    fix action

---
+++ .github/actions/setup/index.js
@@ -0,0 +1,15 @@
+const core = require("@actions/core");
+const exec = require("@actions/exec");
+
+async function run() {
+  try {
+    const nodeVersion = core.getInput("node-version");
+    await exec.exec(`nvm install ${nodeVersion}`);
+    await exec.exec(`nvm use ${nodeVersion}`);
+    await exec.exec(`nvm ci`);
+  } catch (error) {
+    core.setFailed(error.message);
+  }
+}
+
+run();
