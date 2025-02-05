e537cf975f783ae7fe4a442e02262989c39a09af
MCLemuri <elefine@mail.de>
2025-02-05 15:45:59 +0100

    add InDevelopmentPage

--- src/components/RoutingComponent/RoutingComponent.tsx
+++ src/components/RoutingComponent/RoutingComponent.tsx
@@ -1,6 +1,7 @@
 import PageLayout from "@components/PageLayout/PageLayout";
 import AthleteOverviewPage from "@pages/Athletes/AthleteOverviewPage";
 import HomePage from "@pages/Home/HomePage";
+import InDevelopmentPage from "@pages/InDevelopment/InDevelopmentPage";
 import NotFoundPage from "@pages/NotFound/NotFoundPage";
 import { Route, Routes } from "react-router";
 
@@ -10,6 +11,14 @@ const RoutingComponent = () => {
       <Route path="/" element={<PageLayout />}>
         <Route index element={<HomePage />} />
         <Route path="/athletes" element={<AthleteOverviewPage />} />
+        <Route path="/performanceMetric" element={<InDevelopmentPage />} />
+        <Route path="/assignAthlete" element={<InDevelopmentPage />} />
+        <Route path="/dashboard" element={<InDevelopmentPage />} />
+        <Route path="/profile" element={<InDevelopmentPage />} />
+        <Route path="/requirements" element={<InDevelopmentPage />} />
+        <Route path="/performances" element={<InDevelopmentPage />} />
+        <Route path="/downloads" element={<InDevelopmentPage />} />
+        <Route path="/help" element={<InDevelopmentPage />} />
         <Route path="*" element={<NotFoundPage />} />
       </Route>
     </Routes>
