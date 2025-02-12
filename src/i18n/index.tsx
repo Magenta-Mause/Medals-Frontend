3a81c36ed0282013539f64975d154df0df16f458
janne6565 <jabbekeipert@gmail.com>
2025-02-12 16:05:44 +0100

    change variables in backend

--- src/i18n/index.tsx
+++ src/i18n/index.tsx
@@ -2,6 +2,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import germanTranslation from "./de-DE/translation.json";
 import englishTranslation from "./en-US/translation.json";
+import spanishTranslation from "./sp-SP/translation.json";
 
 const resources = {
   en: {
@@ -9,6 +10,9 @@ const resources = {
   },
   de: {
     translation: germanTranslation,
+  },
+  sp: {
+    translation: spanishTranslation,
   },
 };
 
