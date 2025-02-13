8dfda423a839a9dc6f054b30dbc234b03d0487d8
Janne <jabbekeipert@gmail.com>
2025-02-13 11:42:10 +0100

    fix things

--- src/i18n/index.tsx
+++ src/i18n/index.tsx
@@ -2,7 +2,7 @@ import i18n from "i18next";
 import { initReactI18next } from "react-i18next";
 import germanTranslation from "./de-DE/translation.json";
 import englishTranslation from "./en-US/translation.json";
-import spanishTranslation from "./sp-SP/translation.json";
+import spanishTranslation from "./es-ES/translation.json";
 
 const resources = {
   en: {
