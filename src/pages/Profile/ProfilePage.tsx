ec64e11c2c193279389c209396ccf0b1c1a94d7b
MCLemuri <elefine@mail.de>
2025-02-19 15:51:33 +0100

    add base profile info

--- src/pages/Profile/ProfilePage.tsx
+++ src/pages/Profile/ProfilePage.tsx
@@ -1,5 +1,5 @@
 import AthleteDatagrid from "@components/Datagrids/AthleteDatagrid/AthleteDatagrid";
-import { Box, Card, CardContent, Typography } from "@mui/joy";
+import { Box, Button, Card, CardContent, Typography } from "@mui/joy";
 import { useTypedSelector } from "@stores/rootReducer";
 import { useTranslation } from "react-i18next";
 import { useContext } from "react";
@@ -10,7 +10,28 @@ const ProfilePage = () => {
   const athletes = useTypedSelector((state) => state.athletes.data);
   const athleteState = useTypedSelector((state) => state.athletes.state);
   const { selectedUser } = useContext(AuthContext);
+  const selectedAthlete = athletes.find((athlete: { id: number | undefined; }) => athlete.id === selectedUser?.id);
   const { t } = useTranslation();
+  
+  const formattedDate = new Intl.DateTimeFormat("de-DE", {
+    day: "2-digit",
+    month: "2-digit",
+    year: "numeric"
+  }).format(new Date(selectedAthlete?.birthdate));
+
+  let gender = '';
+
+  // Setze den Wert für gender abhängig vom Geschlecht
+  //if (selectedAthlete?.gender === "MALE") {
+    //gender = "männlich";
+  //} else if (selectedAthlete?.gender === "FEMALE") {
+    //gender = "weiblich";
+  //} else if (selectedAthlete?.gender === "DIVERSE") {
+   // gender = "divers";
+  //}
+  
+  
+
 
   return (
     <>
@@ -30,21 +51,33 @@ const ProfilePage = () => {
         </Typography>
       </Box>
       <Box sx={{
-              width: "100%",
-              maxWidth: "350px",
-              flexGrow: 1,
-            }}
-          >
-        <Card variant="outlined">
-          <CardContent>
-            {selectedUser?.first_name} {selectedUser?.last_name} <br />
-            ID: {selectedUser?.id} <br />
-            Birthdate: {}
-            Email: {selectedUser?.email}
-          </CardContent>
-        </Card>
-      </Box>
-      
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+      }}>
+        <Box sx={{ width: "100%", maxWidth: "350px" }}>
+          <Typography level="h2" component="h1">
+            {selectedUser?.first_name} {selectedUser?.last_name}
+          </Typography>
+          <Card variant="outlined">
+            
+            <CardContent>
+              ID: {selectedUser?.id} <br />
+              {t("pages.profilePage.birthdate")}: {formattedDate} <br />
+              {t("pages.profilePage.gender")}: {t("genders." + selectedAthlete.gender)} <br />
+              {t("pages.profilePage.email")}: {selectedUser?.email}
+            </CardContent>
+          </Card>
+        </Box>
+
+        {/* Buttons unter der Card */}
+        <Box sx={{ display: "flex", flexDirection: "column", gap: 2, mt: 2 }}>
+          <Button variant="outlined">Reset Password</Button>
+          <Button variant="outlined">Delete Profile</Button>
+        </Box>
+      </Box>   
     </>
   );
 };
