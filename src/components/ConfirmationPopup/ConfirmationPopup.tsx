ee23cd8261f75b0bc8c53465f163f3f1a95706ea
MCLemuri <elefine@mail.de>
2025-02-20 16:18:17 +0100

    add ConfirmationPopup formatting

--- src/components/ConfirmationPopup/ConfirmationPopup.tsx
+++ src/components/ConfirmationPopup/ConfirmationPopup.tsx
@@ -1,5 +1,6 @@
 import React from 'react';
-import { Box, Button, Modal, Typography, Sheet } from '@mui/joy';
+import { Box, Button, Modal, Typography, Sheet, Card } from '@mui/joy';
+import { useTranslation } from 'react-i18next';
 
 interface ConfirmationPopupProps {
   open: boolean;
@@ -9,36 +10,35 @@ interface ConfirmationPopupProps {
 }
 
 const ConfirmationPopup: React.FC<ConfirmationPopupProps> = ({ open, onClose, onConfirm, message }) => {
+  const { t } = useTranslation();
   return (
     <Modal open={open} onClose={onClose}>
-      <Sheet
-        variant="outlined"
-        sx={{
-          minWidth: 300,
-          margin: 'auto',
-          padding: 2,
-          display: 'flex',
-          flexDirection: 'column',
-          alignItems: 'center',
-          justifyContent: 'center',
-          gap: 2
-        }}
-      >
+      <Box sx={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        height: "100vh",
+        
+      }}>
+      <Card sx={{ width: '50%', minWidth: "275px"}}>
         <Typography level="h4" component="h2" sx={{ textAlign: 'center' }}>
-          Bestätigung erforderlich
+          {t("components.confirmationPopup.header")}
         </Typography>
         <Typography sx={{ textAlign: 'center' }}>
           {message}
         </Typography>
-        <Box sx={{ display: 'flex', gap: 2 }}>
+        <Box sx={{ display: 'flex', gap: 2, justifyContent: "center", width: "100%"}}>
           <Button variant="outlined" onClick={onClose}>
-            Abbrechen
+            {t("components.confirmationPopup.cancelButton")}
           </Button>
           <Button variant="outlined" color="danger" onClick={onConfirm}>
-            Löschen
+          {t("components.confirmationPopup.deleteButton")}
+
           </Button>
         </Box>
-      </Sheet>
+    </Card>
+    </Box>
     </Modal>
   );
 };
