f6f6c92f214e1b9cf6fd6479ee8092ca8d820b74
fietensen <fietensen@gmail.com>
2025-02-20 17:43:01 +0100

    refactor redux

--- src/stores/slices/trainerSlice.tsx
+++ src/stores/slices/trainerSlice.tsx
@@ -1,42 +1,33 @@
-import { Athlete } from "@customTypes/bffTypes";
+import { Trainer } from "@customTypes/bffTypes";
 import { createSlice, PayloadAction } from "@reduxjs/toolkit";
 
 interface TrainerSliceState {
-  data: Athlete[];
+  data: Trainer[];
   state: "idle" | "loading" | "failed";
   error: string | null;
 }
 
 const trainerSlice = createSlice({
-  name: "athleteSlice",
+  name: "trainerSlice",
   initialState: {
     data: [],
     state: "idle",
     error: null,
   } as TrainerSliceState,
   reducers: {
-    addTrainer(state, action: PayloadAction<Athlete>) {
+    addTrainer(state, action: PayloadAction<Trainer>) {
       state.data.push(action.payload);
-    },
-    updateTrainer(state, action: PayloadAction<Athlete>) {
-      const index = state.data.findIndex(
-        (athlete) => athlete.id == action.payload.id,
-      );
-      if (index !== -1) {
-        state.data[index] = action.payload;
-      }
     },
     removeTrainer(state, action: PayloadAction<{ id: number }>) {
       state.data = state.data.filter((item) => item.id !== action.payload.id);
     },
-    setTrainers(state, action: PayloadAction<Athlete[]>) {
+    setTrainers(state, action: PayloadAction<Trainer[]>) {
       state.data = action.payload;
     },
   },
 });
 
-const { addTrainer, updateTrainer, removeTrainer, setTrainers } =
-  trainerSlice.actions;
+const { addTrainer, removeTrainer, setTrainers } = trainerSlice.actions;
 
-export { addTrainer, removeTrainer, setTrainers, updateTrainer };
+export { addTrainer, removeTrainer, setTrainers };
 export default trainerSlice.reducer;
