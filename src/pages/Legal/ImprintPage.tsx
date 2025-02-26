714b056104bf9fe670c1e4e8733a20283799a240
py-bay <github.com.tribunal325@passmail.net>
2025-02-26 23:48:46 +0100

    add imprint page

---
+++ src/pages/Legal/ImprintPage.tsx
@@ -0,0 +1,28 @@
+import { Box, Typography } from "@mui/joy";
+import { useTranslation } from "react-i18next";
+
+const ImprintPage = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Box sx={{ p: 3 }}>
+      <Typography level="h1" sx={{ mb: 2 }}>
+        {t("pages.imprintPage.title")}
+      </Typography>
+      <Typography level="body-sm" sx={{ mb: 2 }}>
+        Simon Dietrich
+      </Typography>
+      <Typography level="body-sm" sx={{ mb: 2 }}>
+        Am Pulverturm 54, 01705 Freital, Deutschland
+      </Typography>
+      <Typography level="h2" sx={{ mb: 2 }}>
+        {t("pages.imprintPage.contact")}
+      </Typography>
+      <Typography level="body-sm" sx={{ mb: 2 }}>
+        Email: medals.ageless325@passmail.net
+      </Typography>
+    </Box>
+  );
+};
+
+export default ImprintPage;
