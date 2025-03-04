937433585012a7f47731e9250c4b7dd432654bcc
py-bay <github.com.tribunal325@passmail.net>
2025-03-04 16:50:14 +0100

    add PrivacyPolicyModal to show it on loginpage

---
+++ src/components/PrivacyPolicyModal/PrivacyPolicyModal.tsx
@@ -0,0 +1,26 @@
+import { Modal, ModalDialog, Typography, IconButton } from "@mui/joy";
+import { Close } from "@mui/icons-material";
+import PrivacyPolicyPage from "@pages/Legal/PrivacyPolicyPage";
+
+const PrivacyPolicyModal = ({ open, onClose }: { open: boolean; onClose: () => void }) => {
+  return (
+    <Modal open={open} onClose={onClose}>
+      <ModalDialog
+        aria-labelledby="privacy-policy-modal-title"
+        aria-describedby="privacy-policy-modal-description"
+        sx={{ maxWidth: "90vw", maxHeight: "90vh" }}
+      >
+        <IconButton
+          aria-label="Close"
+          onClick={onClose}
+          sx={{ position: "absolute", top: "1rem", right: "1rem" }}
+        >
+          <Close />
+        </IconButton>
+        <PrivacyPolicyPage />
+      </ModalDialog>
+    </Modal>
+  );
+};
+
+export default PrivacyPolicyModal;
--- src/pages/Login/LoginForm.tsx
+++ src/pages/Login/LoginForm.tsx
@@ -12,6 +12,7 @@ import {
 import { useTranslation } from "react-i18next";
 import { useNavigate } from "react-router";
 import { useState } from "react";
+import PrivacyPolicyModal from "@components/PrivacyPolicyModal/PrivacyPolicyModal";
 
 interface SignInFormElement extends HTMLFormElement {
   readonly elements: FormElements;
@@ -34,71 +35,76 @@ const LoginForm = (props: {
   const navigate = useNavigate();
   const { t } = useTranslation();
   const [privacyPolicyChecked, setPrivacyPolicyChecked] = useState(false);
+  const [privacyPolicyModalOpen, setPrivacyPolicyModalOpen] = useState(false);
 
   return (
-    <form
-      onSubmit={(event: React.FormEvent<SignInFormElement>) => {
-        event.preventDefault();
-        const formElements = event.currentTarget.elements;
-        const data = {
-          email: formElements.email.value,
-          password: formElements.password.value,
-          privacyPolicy: formElements.privacyPolicy.checked,
-        };
+    <>
+      <form
+        onSubmit={(event: React.FormEvent<SignInFormElement>) => {
+          event.preventDefault();
+          const formElements = event.currentTarget.elements;
+          const data = {
+            email: formElements.email.value,
+            password: formElements.password.value,
+            privacyPolicy: formElements.privacyPolicy.checked,
+          };
 
-        props.loginCallback(data);
-      }}
-    >
-      <FormControl required>
-        <FormLabel>{t("pages.loginPage.signIn.input.email")}</FormLabel>
-        <Input type="email" name="email" />
-      </FormControl>
-      <FormControl required>
-        <FormLabel>{t("pages.loginPage.signIn.input.password")}</FormLabel>
-        <Input type="password" name="password" />
-      </FormControl>
-      <Stack sx={{ gap: 4, mt: 2 }}>
-        <Box
-          sx={{
-            display: "flex",
-            justifyContent: "space-between",
-            alignItems: "center",
-          }}
-        >
-          <Checkbox
-            size="sm"
-            name="privacyPolicy"
-            checked={privacyPolicyChecked}
-            onChange={(e) => setPrivacyPolicyChecked(e.target.checked)}
-          />
-          <Typography level="body-sm">
-            <Link
-              level="body-sm"
-              onClick={() => {
-                navigate("/privacyPolicy");
-              }}
-            >
-              {t("pages.loginPage.signIn.privacyPolicy")}
-            </Link>
-          </Typography>
-          <Link
-            level="title-sm"
-            onClick={() => {
-              navigate("/resetPassword");
+          props.loginCallback(data);
+        }}
+      >
+        <FormControl required>
+          <FormLabel>{t("pages.loginPage.signIn.input.email")}</FormLabel>
+          <Input type="email" name="email" />
+        </FormControl>
+        <FormControl required>
+          <FormLabel>{t("pages.loginPage.signIn.input.password")}</FormLabel>
+          <Input type="password" name="password" />
+        </FormControl>
+        <Stack sx={{ gap: 4, mt: 2 }}>
+          <Box
+            sx={{
+              display: "flex",
+              justifyContent: "space-between",
+              alignItems: "center",
             }}
           >
-            {t("pages.loginPage.signIn.forgotPassword")}
-          </Link>
-        </Box>
-        <Button type="submit" fullWidth disabled={props.isPending}>
-          {!props.isPending ? (
-            t("pages.loginPage.signIn.submit")
-          ) : (
-            <>{t("pages.loginPage.signIn.loading")}</>
-          )}
-        </Button>
-      </Stack>
-    </form>
+            <Checkbox
+              size="sm"
+              name="privacyPolicy"
+              checked={privacyPolicyChecked}
+              onChange={(e) => setPrivacyPolicyChecked(e.target.checked)}
+            />
+            <Typography level="body-sm">
+              <Link
+                level="body-sm"
+                onClick={() => setPrivacyPolicyModalOpen(true)}
+              >
+                {t("pages.loginPage.signIn.privacyPolicy")}
+              </Link>
+            </Typography>
+            <Link
+              level="title-sm"
+              onClick={() => {
+                navigate("/resetPassword");
+              }}
+            >
+              {t("pages.loginPage.signIn.forgotPassword")}
+            </Link>
+          </Box>
+          <Button type="submit" fullWidth disabled={props.isPending || !privacyPolicyChecked}>
+            {!props.isPending ? (
+              t("pages.loginPage.signIn.submit")
+            ) : (
+              <>{t("pages.loginPage.signIn.loading")}</>
+            )}
+          </Button>
+        </Stack>
+      </form>
+      <PrivacyPolicyModal
+        open={privacyPolicyModalOpen}
+        onClose={() => setPrivacyPolicyModalOpen(false)}
+      />
+    </>
   );
 };
 
