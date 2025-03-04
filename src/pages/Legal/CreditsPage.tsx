087095cabfce28fd1decaaf28f38a4092de32edd
py-bay <github.com.tribunal325@passmail.net>
2025-03-04 14:56:45 +0100

    add i18n to CreditsPage

--- src/pages/Legal/CreditsPage.tsx
+++ src/pages/Legal/CreditsPage.tsx
@@ -6,56 +6,56 @@ const CreditsPage = () => {
 
   const imageCredits = [
     {
-      description: t("pages.creditsPage.loginPage"),
+      page: t("pages.creditsPage.loginPage"),
       credits: [
         {
-          mode: "LightMode",
-          text: "Photo by Anastasia Shuraeva",
+          description: t("pages.creditsPage.lightMode"),
+          author: "Anastasia Shuraeva",
           link: "https://www.pexels.com/photo/coach-talking-to-the-athletes-9519495/",
         },
         {
-          mode: "DarkMode",
-          text: "Photo by Rosemary Ketchum",
+          description: t("pages.creditsPage.darkMode"),
+          author: "Rosemary Ketchum",
           link: "https://www.pexels.com/photo/group-of-women-standing-outdoor-during-day-1564420/",
         },
       ],
     },
     {
-      description: t("pages.creditsPage.downloadPage"),
+      page: t("pages.creditsPage.downloadPage"),
       credits: [
         {
-          mode: "SwimmingCard",
-          text: "Photo by Mali Meader",
+          description: t("pages.creditsPage.swimmingCard"),
+          author: "Mali Meader",
           link: "https://www.pexels.com/photo/person-swimming-on-body-of-water-1415810/",
         },
         {
-          mode: "SingleAtheletsCard",
-          text: "Photo by Nappy",
+          description: t("pages.creditsPage.singleAthletesCard"),
+          author: "Nappy",
           link: "https://www.pexels.com/photo/man-wearing-white-sweater-and-black-shorts-about-to-run-936094/",
         },
         {
-          mode: "GroupAtheletsCard",
-          text: "Photo by Andrea Piacquadio",
+          description: t("pages.creditsPage.groupAthletesCard"),
+          author: "Andrea Piacquadio",
           link: "https://www.pexels.com/photo/basketball-team-stacking-hands-together-3755440/",
         },
         {
-          mode: "2020MedalsCard",
-          text: "Photo by DS stories",
+          description: t("pages.creditsPage.2020MedalsCard"),
+          author: "DS stories",
           link: "https://www.pexels.com/photo/medals-on-a-beige-surface-7267586/",
         },
         {
-          mode: "2023MedalsCard",
-          text: "Photo by DS stories",
+          description: t("pages.creditsPage.2023MedalsCard"),
+          author: "DS stories",
           link: "https://www.pexels.com/photo/photograph-of-medals-on-white-shapes-7267573/",
         },
         {
-          mode: "2024MedalsCard",
-          text: "Photo by DS stories",
+          description: t("pages.creditsPage.2024MedalsCard"),
+          author: "DS stories",
           link: "https://www.pexels.com/photo/gold-silver-and-bronze-round-medals-7267538/",
         },
         {
-          mode: "2025MedalsCard",
-          text: "Photo by DS stories",
+          description: t("pages.creditsPage.2025MedalsCard"),
+          author: "DS stories",
           link: "https://www.pexels.com/photo/different-types-of-medals-on-shapes-7267585/",
         },
       ],
@@ -68,26 +68,27 @@ const CreditsPage = () => {
         overflowY: "scroll",
         height: "100vh",
         pb: 2,
-      }}>
+      }}
+    >
       <Typography level="h1" sx={{ mb: 2 }}>
         {t("pages.creditsPage.title")}
       </Typography>
       {imageCredits.map((section, index) => (
         <Box key={index} sx={{ mb: 3 }}>
           <Typography level="h3" sx={{ mb: 1 }}>
-            {section.description}
+            {section.page}
           </Typography>
           <List>
             {section.credits.map((credit, idx) => (
               <ListItem key={idx}>
                 <Typography>
-                  {credit.mode}:{" "}
+                  {credit.description}:{" "}
                   <a
                     href={credit.link}
                     target="_blank"
                     rel="noopener noreferrer"
                   >
-                    {credit.text}
+                    {t("pages.creditsPage.photoBy")} {credit.author}
                   </a>
                 </Typography>
               </ListItem>
