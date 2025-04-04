8b091e31711fc2c3f112e439c4ece51fdf8fb8cc
janne6565 <jabbekeipert@gmail.com>
2025-04-04 18:01:38 +0200

    add logging

--- src/components/datagrids/DisciplineDatagrid/DisciplineDatagrid.tsx
+++ src/components/datagrids/DisciplineDatagrid/DisciplineDatagrid.tsx
@@ -43,6 +43,23 @@ const DisciplineDatagrid = (props: DisciplineDatagridProps) => {
       };
     });
     console.log(updated);
+    console.log(
+      updated.sort(
+        (a, b) =>
+          Math.max(
+            0,
+            ...b.performanceRecordings.map((p) =>
+              parseInt(Date.parse(p.date_of_performance).toFixed()),
+            ),
+          ) -
+          Math.max(
+            0,
+            ...a.performanceRecordings.map((p) =>
+              parseInt(Date.parse(p.date_of_performance).toFixed()),
+            ),
+          ),
+      ),
+    );
     setData(updated);
   }, [props.performanceRecordings, props.disciplines]);
 
