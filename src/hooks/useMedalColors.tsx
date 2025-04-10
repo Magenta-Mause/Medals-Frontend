ef4baba9cca5f177c4c5e437a7654b570598b989
py-bay <github.com.tribunal325@passmail.net>
2025-04-11 00:45:53 +0200

    improve medal colors

--- src/hooks/useMedalColors.tsx
+++ src/hooks/useMedalColors.tsx
@@ -2,11 +2,11 @@ import { useTheme } from "@mui/joy/styles";
 
 export const useMedalColors = () => {
   const theme = useTheme();
-  const isDarkMode = theme.palette.mode === "dark";
+  const isLightMode = theme.palette.mode === "light";
 
   return {
-    gold: isDarkMode ? "#FFEB00" : "#FFD700",
-    silver: isDarkMode ? "#EEEEEE" : "#dceff6",
-    bronze: isDarkMode ? "#D84315" : "#CD7F32",
+    gold: isLightMode ? "#FFD700" : "#FFD700",
+    silver: isLightMode ? "#C0C0C0" : "#C0C0C0",
+    bronze: isLightMode ? "#CD7F32" : "#CD7F32",
   };
 };
