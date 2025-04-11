1ff615e0807f55e0f937885c581bab72842b88d6
janne6565 <jabbekeipert@gmail.com>
2025-04-11 10:15:22 +0200

    add dark mode swim certificate color

--- src/components/icons/SwimCertificateIcon/SwimCertificateIcon.tsx
+++ src/components/icons/SwimCertificateIcon/SwimCertificateIcon.tsx
@@ -18,7 +18,11 @@ const SwimCertificateIcon: React.FC<SwimCertificateIconProps> = ({
   const { mode } = useColorScheme();
   const unachievedBackgroundColor =
     mode === "dark" ? "rgba(255, 255, 255, 0.3)" : "rgba(0, 0, 0, 0.2)";
-  const backgroundColor = achieved ? "#82afd0" : unachievedBackgroundColor;
+  const backgroundColor = achieved
+    ? mode === "light"
+      ? "#82afd0"
+      : "#376788"
+    : unachievedBackgroundColor;
   const border = achieved ? "gray solid thin" : "rgba(0,0,0,0.2) solid thin";
   const detailColor =
     mode === "dark" ? "rgba(255, 255, 255, 1)" : "rgba(0, 0, 0, 0.5)";
