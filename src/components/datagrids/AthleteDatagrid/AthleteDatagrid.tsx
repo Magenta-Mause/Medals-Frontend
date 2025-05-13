92dfdfe836470c5f4fc2b15b76c311f042b45510
py-bay <104859696+py-bay@users.noreply.github.com>
2025-05-13 12:46:29 +0200

    [MED-187] UI improvements for athlete data entry and login page (#64)
    * show only informational message about data privacy insteasd of a checkbox
    
    * fix build error
    
    * moile view adjustment: move medals into a new row and action buttons ito top right 3-dot menu
    
    * use existing RowMenu instead of own implementation
    
    * Improve dropdown UI spacing and display available disciplines first and move date selector to top of form
    
    * remove dot menu from mobile view in DisciplineDatagrid because details is the only option and also available through component click
    
    * move privacy policy hint below login button and decrease font size
    
    * beautify discipline selection in createPerformanceRecordingModal
    
    * fix lint
    
    * add delete option to mobile view performance recording view for trainers
    
    * fix lint
    
    * add delete option for trainer mobile view for performance recording
    
    * remove additionalActions
    
    * fix: adjusted the divider and section header
    
    * fix: header of the section is lighter and margin increased
    
    * Revert "fix: header of the section is lighter and margin increased"
    
    This reverts commit 7bedf389254f3faffebe544e43e718cef56f2f06.
    
    * fix: header of the section is lighter and margin increased
    
    * fix: had git problems now the changes where applied
    
    ---------
    
    Co-authored-by: hyunjoonchoi <hyunjoonchoi04@gmail.com>

--- src/components/datagrids/AthleteDatagrid/AthleteDatagrid.tsx
+++ src/components/datagrids/AthleteDatagrid/AthleteDatagrid.tsx
@@ -323,7 +323,7 @@ const AthleteDatagrid = (props: AthleteDatagridProps) => {
         {formatLocalizedDate(athlete.birthdate)}
       </Typography>
     ),
-    bottomButtons: [
+    topRightMenu: [
       {
         key: "openDetails",
         label: t("components.athleteDatagrid.actions.openDetails"),
@@ -332,7 +332,7 @@ const AthleteDatagrid = (props: AthleteDatagridProps) => {
       },
       ...actions.filter((action) => action.key !== "export"),
     ],
-    topRightInfo: (athlete) => (
+    contentRow: (athlete) => (
       <AchievementsBox
         athlete={athlete}
         performanceRecordings={performanceRecordings}
