5cec31409711661f4079d712ea329b4a021041f0
Hyunjoon <104907396+joonjester@users.noreply.github.com>
2025-05-15 08:31:09 +0200

    [MED-148] Added info modal at login (#73)
    * feature: added info modal at login
    
    * fix: changed the info link to an icon
    
    * fix: increase modal size, changed link button and i18n
    
    * fix: lint and i18n
    
    * fix: spacing between header and link
    
    * fix: go rid of unnecassery tag
    
    * fix: text width and height

---
+++ src/components/modals/InfoAtLoginModal/InfoAtLoginModal.tsx
@@ -0,0 +1,43 @@
+import { Box, Typography } from "@mui/joy";
+import { useTranslation } from "react-i18next";
+import GenericModal from "../GenericModal";
+
+const InfoAtLoginModal = ({
+  open,
+  setOpen: setOpen,
+}: {
+  open: boolean;
+  setOpen: (open: boolean) => void;
+}) => {
+  const { t } = useTranslation();
+
+  return (
+    <GenericModal
+      open={open}
+      setOpen={setOpen}
+      header={t("pages.loginPage.info.header")}
+      modalDialogSX={{ maxWidth: "40vw", maxHeight: "55vh" }}
+      modalSX={{ left: 0 }}
+    >
+      <Box>
+        <Typography level="h3" sx={{ mb: 2 }}>
+          {t("pages.loginPage.info.sectionHeader")}
+        </Typography>
+        <Typography level="body-md" sx={{ mb: 2, white: "pre-line" }}>
+          {t("pages.loginPage.info.greeting")}.
+        </Typography>
+        <Box component="ul" sx={{ pl: 3, mb: 2 }}>
+          <li>{t("pages.loginPage.info.admin")}</li>
+          <li>{t("pages.loginPage.info.trainer")}</li>
+          <li>{t("pages.loginPage.info.athlete")}</li>
+        </Box>
+        <Typography level="body-md" sx={{ mb: 2, whiteSpace: "pre-line" }}>
+          {t("pages.loginPage.info.content")}
+          <u>medals.ageless325@passmail.net</u>
+        </Typography>
+      </Box>
+    </GenericModal>
+  );
+};
+
+export default InfoAtLoginModal;
