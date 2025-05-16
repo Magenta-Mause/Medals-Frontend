50fa930a0add794fec0c7e4cba2c46e7cc3d4f11
janne6565 <jabbekeipert@gmail.com>
2025-05-16 09:47:34 +0200

    add breadcrumbs back on start page

--- src/components/CustomBreadcrumbs/CustomBreadcrumbs.tsx
+++ src/components/CustomBreadcrumbs/CustomBreadcrumbs.tsx
@@ -4,12 +4,11 @@ import useBreadcrumbs, {
 } from "use-react-router-breadcrumbs";
 import { Breadcrumbs, Link } from "@mui/joy";
 import { ChevronRight } from "@mui/icons-material";
-import { Link as RouterLink, useLocation } from "react-router";
+import { Link as RouterLink } from "react-router";
 import { useTranslation } from "react-i18next";
 import useAthleteLookup from "@hooks/useAthleteLookup";
 
 const CustomBreadcrumbs = () => {
-  const location = useLocation();
   const { t } = useTranslation();
   const { useAthleteNameLookup } = useAthleteLookup();
 
@@ -27,32 +26,28 @@ const CustomBreadcrumbs = () => {
 
   return (
     <>
-      {location.pathname == "/" ? (
-        <></>
-      ) : (
-        <>
-          <Breadcrumbs
-            size="sm"
-            aria-label="breadcrumbs"
-            separator={<ChevronRight />}
-            sx={{ pl: 0 }}
-          >
-            {breadcrumbs.map(({ match, breadcrumb }) => (
-              <Link
-                component={RouterLink}
-                to={match.pathname}
-                key={breadcrumb?.toLocaleString()}
-              >
-                {t("components.breadcrumbs.paths." + match.pathname, {
-                  defaultValue: "",
-                }) == ""
-                  ? breadcrumb
-                  : t("components.breadcrumbs.paths." + match.pathname)}
-              </Link>
-            ))}
-          </Breadcrumbs>
-        </>
-      )}
+      <>
+        <Breadcrumbs
+          size="sm"
+          aria-label="breadcrumbs"
+          separator={<ChevronRight />}
+          sx={{ pl: 0 }}
+        >
+          {breadcrumbs.map(({ match, breadcrumb }) => (
+            <Link
+              component={RouterLink}
+              to={match.pathname}
+              key={breadcrumb?.toLocaleString()}
+            >
+              {t("components.breadcrumbs.paths." + match.pathname, {
+                defaultValue: "",
+              }) == ""
+                ? breadcrumb
+                : t("components.breadcrumbs.paths." + match.pathname)}
+            </Link>
+          ))}
+        </Breadcrumbs>
+      </>
     </>
   );
 };
